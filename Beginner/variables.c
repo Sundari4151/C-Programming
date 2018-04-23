5041d3069ec5896aa5856864c6ba02d86714e1f8
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-04-23 10:10:04 +0000

    Create variables.c

---
+++ Beginner/variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int main ()
+
+{
+
+   int a, b;
+   char c[25]="Sachin Ramesh Tendullk";
+   char d[25]="Little master";
+   float f;
+   a = 18426;
+   f = 44.83;
+   b = 463;
+   printf("%s\n",c);
+   printf("%s\n",d);
+   printf("%d\n",a);
+   printf("%f\n",f);
+   printf("%d\n",b);
+   return 0;
+
+}

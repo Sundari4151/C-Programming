3d3c56583d7d3baf72521d1c02a17e28f3911791
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-04-24 05:04:01 +0000

    Create operators.c

---
+++ player/operators.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ int a=5,b=5;
+
+ int c,d,e,f,g;
+
+ c=a+b;
+ d=a-b;
+ e=a*b;
+ f=a/b;
+ g=a%b;
+
+ printf("Add:%d\n",c);
+ printf("Subtract:%d\n",d);
+ printf("Mulitiply:%d\n",e);
+ printf("Division:%d\n",f);
+ printf("Modulo:%d\n",g);
+ 
+
+}

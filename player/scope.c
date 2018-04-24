ff97bdf997da387fe24bc281d1e34bfd4249c571
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-04-24 05:02:56 +0000

    Create scope.c

---
+++ player/scope.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int global = 100;          // global variable declared
+
+void main()
+
+{
+
+   int local = 10;         // local variable declared
+
+   printf("Global variable is %d\n",global);
+
+   printf("Local variable is %d\n",local);
+
+   
+
+}

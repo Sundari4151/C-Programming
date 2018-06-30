e528e8783099ef552ab20ca18828874315359576
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-06-30 07:04:03 +0000

    Create nkarray.c

---
+++ codekata/set 1/nkarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int b[20],N,K,sum=0;
+  printf("%d",N);
+  printf("%d',&K);
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&b[i]);
+  }
+  for(i=0;i<K;i++)
+  {
+    sum=sum+b[i];
+  }
+  printf("%d",sum);
+  return 0;
+ }
+    

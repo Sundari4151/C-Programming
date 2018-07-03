b1aad33d8b1ed7d3090bd76bcfe44bf1bfcc41d5
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-07-03 10:50:17 +0000

    Create odd.c

---
+++ codekata/SET 2/odd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int num,m =1,n=5;
+
+
+    printf("Print Odd Numbers in a given range m to n:\n");
+
+    for (num = m; num <n; num++)
+
+        {    if (num % 2 == 1)
+
+            
+
+                 printf ("%d ", num);
+
+         }
+
+                return 0;
+
+}

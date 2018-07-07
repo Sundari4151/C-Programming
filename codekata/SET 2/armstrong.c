948425ac567cfe38ff2bcd19332e90367cee6f7e
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-07-07 06:32:44 +0000

    Create armstrong.c

---
+++ codekata/SET 2/armstrong.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int n,orgnumber,rem,res=0;
+  scanf("%d",&n);
+  while(orgnumber!=0)
+  {
+    rem=orgnumber%10;
+    res=res+rem*rem*rem;
+    orgnumber=orgnumber/10;
+  }
+  if(res==n)
+    printf("yes");
+  else
+    printf("no");
+    return 0;
+  }  

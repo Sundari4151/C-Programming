b3e78fe79e6e6b97c446b549acb7d0b01dd4a4e5
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-07-10 09:31:33 +0000

    Update factorial.c

--- codekata/SET 2/factorial.c
+++ codekata/SET 2/factorial.c
@@ -1,1 +1,20 @@
-
+#iclude<stdio.h>
+int main()
+{
+    int n,i;
+    unsigned long long factorial =1;
+    scanf("%d",&n);
+    if(n<o)
+    {
+       printf("Error");
+    }
+    else
+    {
+        for(i=1;i<=n;++i)
+        {
+            factorial*=1;
+        }
+     printf("%llu",factorial);
+    }
+      return 0;
+}

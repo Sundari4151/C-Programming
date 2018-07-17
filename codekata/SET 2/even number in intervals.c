2b525c48aa78287fcce73957faf2e9ffaefdd89b
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-07-17 10:44:40 +0000

    Update even number in intervals.c

--- codekata/SET 2/even number in intervals.c
+++ codekata/SET 2/even number in intervals.c
@@ -1,32 +1,18 @@
-#include<stdio.h> 
-
-int main(){
-
+#include<stdio.h>
+int main()
+{
            int f1,f2,rem,i;
-
-           
-
            scanf("%d",&f1);
-
-           
-
            scanf("%d",&f2);
-
            if(f1%2==0)
            {
-           f1=f1+1;
+                      f1=f1+1;
            }
-           for(i=f1; i<f2; i++)
+           for(i=f1;i<f2;++i)
            {
-
-               rem = i % 2;
-
-               if(rem == 0)
-
-               printf("\n  %d",i);
-
+                      rem=i%2;
+                      if(rem==0)
+                                 printf("%d",i);
            }
-
-    return 0; 
-
-    } 
+           return 0;
+}

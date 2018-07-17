bb2831c0af03438bbf1f05eca2c3bb623d376f74
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-07-17 11:15:28 +0000

    Create primenumber in intervals.c

---
+++ codekata/SET 2/primenumber in intervals.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int f1,f2,i,j,flag;
+  scanf("%d%d",&f1,&f2);
+  for(i=n1+1;i<n2;++i)
+  {
+    flag=0;
+    for(j=2;j=i/2;++j)
+    {
+      if(i%j==0)
+      {
+        flag=1;
+        break;
+      }
+    }
+      if(flag==0)
+        printf("%d",i);
+        
+    }
+    return 0;
+}

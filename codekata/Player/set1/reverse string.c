c2027935281d629e7a7bbe986424cd2f2869536f
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-08-04 07:20:25 +0000

    Create reverse string.c

---
+++ codekata/Player/set1/reverse string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char Str[100];
+  int i,len;
+  gets(str);
+  len=strlen(Str);
+  for(i=len-1;i>=0;i--)
+  {
+    printf("%c",Str[i]);
+   }
+   return 0;
+ }

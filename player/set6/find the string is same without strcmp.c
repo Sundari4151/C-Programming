4c4c5b59f9824d9706e01df3dd3e0224bf275ec7
Sundari4151 <38653089+Sundari4151@users.noreply.github.com>
2018-09-11 10:21:23 +0000

    Create find the string is same without strcmp.c

---
+++ player/set6/find the string is same without strcmp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{ 
+  char str1[15],str2[15];
+  int i,temp=0;
+  scanf("%s",str1);
+  scanf("%d",str2);
+  for(i=0;str[i]!='\0';i++)
+  {
+    if(str1[i]==str2[i])
+      temp=1;
+    else
+      temp=0;
+  }
+  if(temp==1)
+    printf("yes");
+  else
+    printf("no");
+ getch();
+ return 0;
+}
